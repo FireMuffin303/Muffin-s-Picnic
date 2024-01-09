51fbbf6d88add34c4fb63d202260839b325e2c8c
FireMuffin303 <firegame303@gmail.com>
2024-01-09 20:46:28 +0700

    add recipe and texture
    man jumping around editing, mod dev and university project are tiring.

--- fabric/src/main/java/net/firemuffin303/fabric/datagen/RecipeLangDataGen.java
+++ fabric/src/main/java/net/firemuffin303/fabric/datagen/RecipeLangDataGen.java
@@ -7,6 +7,7 @@ import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
 import java.util.function.Consumer;
@@ -19,9 +20,33 @@ public class RecipeLangDataGen extends FabricRecipeProvider {
 
     @Override
     public void buildRecipes(Consumer<FinishedRecipe> exporter) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.WHITE_PICNIC_BASKET,1).define('A', ItemTags.WOODEN_SLABS).define('B',Items.WHITE_CARPET).pattern("ABA").pattern("AAA").unlockedBy(getHasName(Items.WHITE_CARPET),has(Items.WHITE_CARPET)).save(exporter,"crafting/"+getItemName(ModItems.WHITE_PICNIC_BASKET)+"_from_crafting");
-        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.LIGHT_GRAY_PICNIC_BASKET,1).define('A', ItemTags.WOODEN_SLABS).define('B',Items.LIGHT_GRAY_CARPET).pattern("ABA").pattern("AAA").unlockedBy(getHasName(Items.LIGHT_GRAY_CARPET),has(Items.LIGHT_GRAY_CARPET)).save(exporter,"crafting/"+getItemName(ModItems.LIGHT_GRAY_PICNIC_BASKET)+"_from_crafting");
-        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.RED_PICNIC_BASKET,1).define('A', ItemTags.WOODEN_SLABS).define('B',Items.RED_CARPET).pattern("ABA").pattern("AAA").unlockedBy(getHasName(Items.SPRUCE_SLAB),has(Items.SPRUCE_SLAB)).save(exporter,"crafting/"+getItemName(ModItems.RED_PICNIC_BASKET)+"_from_crafting");
+        picnic(ModItems.WHITE_PICNIC_BASKET,Items.WHITE_CARPET,exporter);
+        picnic(ModItems.LIGHT_GRAY_PICNIC_BASKET,Items.LIGHT_GRAY_CARPET,exporter);
+        picnic(ModItems.GRAY_PICNIC_BASKET,Items.GRAY_CARPET,exporter);
+        picnic(ModItems.BLACK_PICNIC_BASKET,Items.BLACK_CARPET,exporter);
+        picnic(ModItems.BROWN_PICNIC_BASKET,Items.BROWN_CARPET,exporter);
+        picnic(ModItems.RED_PICNIC_BASKET,Items.RED_CARPET,exporter);
+        picnic(ModItems.ORANGE_PICNIC_BASKET,Items.ORANGE_CARPET,exporter);
+        picnic(ModItems.YELLOW_PICNIC_BASKET,Items.YELLOW_CARPET,exporter);
+        picnic(ModItems.LIME_PICNIC_BASKET,Items.LIME_CARPET,exporter);
+        picnic(ModItems.GREEN_PICNIC_BASKET,Items.GREEN_CARPET,exporter);
+        picnic(ModItems.CYAN_PICNIC_BASKET,Items.CYAN_CARPET,exporter);
+        picnic(ModItems.LIGHT_BLUE_PICNIC_BASKET,Items.LIGHT_BLUE_CARPET,exporter);
+        picnic(ModItems.BLUE_PICNIC_BASKET,Items.BLUE_CARPET,exporter);
+        picnic(ModItems.PURPLE_PICNIC_BASKET,Items.PURPLE_CARPET,exporter);
+        picnic(ModItems.MAGENTA_PICNIC_BASKET,Items.MAGENTA_CARPET,exporter);
+        picnic(ModItems.PINK_PICNIC_BASKET,Items.PINK_CARPET,exporter);
 
     }
+
+    public void picnic(Item result,Item carpet,Consumer<FinishedRecipe> consumer){
+        ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS,result,1)
+                .define('A',ItemTags.WOODEN_SLABS)
+                .define('B',carpet)
+                .pattern("ABA")
+                .pattern("AAA")
+                .unlockedBy(getHasName(carpet),has(carpet))
+                .save(consumer, "crafting/"+getItemName(result)+"_from_crafting");
+    }
+
 }
